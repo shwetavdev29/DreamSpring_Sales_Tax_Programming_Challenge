b4ae96f024f3ef259711bc1141f2afd5b563b51f
shwetavdev29 <shwetavdev29@gmail.com>
2022-02-03 20:59:31 +0530

    Added jQuery and managed currency coversion with ajax

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,23 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "bootstrap"
+
+import * as bootstrap from "bootstrap"
+import "./src/jquery.js"
+import "jquery-ui"
+
+
+$('select.select-currency').on('focus', function(){
+  var selected = this.selectedOptions[0].value
+  localStorage.setItem("current", selected)
+});
+
+$('select.select-currency').on('click', function(){
+  var selected = this.selectedOptions[0].value
+  const url=`${window.location.href}.js`;
+  $.ajax({
+    type: "GET",
+    url: url,
+    data: { from_currency: localStorage.getItem("current"), to_currency: selected }
+  })
+});
